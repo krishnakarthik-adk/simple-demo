e08db758412045c2df684607d30ec1c611b369b0
Krishna Karthik <krishnakarthik.adk@gmail.com>
2023-08-26 10:45:48 +0530

    a simple rest api end point for saying hello.

---
+++ src/main/java/com/simpledemo/git/DemoController.java
@@ -0,0 +1,15 @@
+package com.simpledemo.git;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/simpledemo")
+public class DemoController {
+	
+	@RequestMapping("/sayHello")
+	public String sayHello() {
+		return "Hello Students!";
+	}
+
+}
