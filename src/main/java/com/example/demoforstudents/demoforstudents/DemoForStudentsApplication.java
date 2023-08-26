e35827f5c3089bd8cb429b6c6bf7818cd4416571
Krishna Karthik <krishnakarthik.adk@gmail.com>
2023-08-26 10:37:37 +0530

    Initial commit.

---
+++ src/main/java/com/example/demoforstudents/demoforstudents/DemoForStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoforstudents.demoforstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoforstudents/demoforstudents/DemoForStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoforstudents.demoforstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
